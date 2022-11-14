3d67ed3ecd48a2b082491f0ee9f0ebec0cd8c3e0
Eric Hooks <ebhooks21@gmail.com>
2022-11-13 20:44:42 -0600

    Started Game module.

---
+++ js/Game.js
@@ -0,0 +1,23 @@
+/**
+ * Game.js -- File to define the game class.
+ */
+class Game
+{
+	/**
+	 * Main constructor for the game class.
+	 */
+	constructor()
+	{
+
+	}
+
+	/**
+	 * Function to start the game.
+	 */
+	start()
+	{
+		alert("HERE2");
+	}
+};
+
+export {Game};
